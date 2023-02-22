8e47643240ad27bd2b530cd278b57ef05747cbe4
klusertim <tim.kluser@gmail.com>
2023-02-22 12:10:45 +0100

    before removing tailwindcss

---
+++ webpagephysioflora/src/components/header/header.data.tsx
@@ -0,0 +1,19 @@
+export default [
+    {
+        path: 'angebot',
+        label: 'Angebot'
+
+    },
+    {
+        paht: 'team',
+        label: 'Team'
+    },
+    {
+        path: 'kontakt',
+        label: 'Kontakt'
+    },
+    {
+        paht: '',
+        label: 'Francais'
+    }
+]
---
+++ webpagephysioflora/src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Header from "@/components/header/header";
+
+export default function Index(){
+
+    return(
+        <Header/>
+    )
+}
